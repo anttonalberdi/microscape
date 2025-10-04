29f69e5e3cfd318a60cce0c09c4061aba11b3293
anttonalberdi <anttonalberdi@gmail.com>
2025-10-04 09:54:53 +0200

    f

--- microscape/space/__init__.py
+++ microscape/space/__init__.py
@@ -1,11 +1,7 @@
-# microscape/space/__init__.py
-from .graph import build_edge_index
 from .diffusion import explicit_diffusion_step, stable_dt_upper_bound
 from .coarsen import coarsen
 
 __all__ = [
-    "Edge",
-    "build_laplacians",
     "explicit_diffusion_step",
     "stable_dt_upper_bound",
     "coarsen",
