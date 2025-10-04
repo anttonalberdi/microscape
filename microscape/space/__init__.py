281ee64a657674019c6bd0ca46d39a85951b3f72
anttonalberdi <anttonalberdi@gmail.com>
2025-10-04 09:55:16 +0200

    f

--- microscape/space/__init__.py
+++
@@ -1,8 +0,0 @@
-from .diffusion import explicit_diffusion_step, stable_dt_upper_bound
-from .coarsen import coarsen
-
-__all__ = [
-    "explicit_diffusion_step",
-    "stable_dt_upper_bound",
-    "coarsen",
-]
